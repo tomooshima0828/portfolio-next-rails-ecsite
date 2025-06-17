4f181f0be8718b3f3b4d183b2689c3911030b0a0
tomooshima0828 <tomo.oshima0828@gmail.com>
2025-06-17 21:55:59 +0900

    feat: Add db:seed on docker-entrypoint and modify seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-User.destroy_all
+if User.count == 0
+  User.create!(
+    name: 'Test User1',
+    email: 'test_user1@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    address: '123 Main St',
+    phone: '123-456-7890'
+  )
+end
 
-User.create!(
-  name: 'Test User1',
-  email: 'test_user1@example.com',
-  password: 'password',
-  password_confirmation: 'password',
-  address: '123 Main St',
-  phone: '123-456-7890'
-)
